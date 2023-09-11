b4193f78ef6c2a3992e78f86a3358eca2261ad96
Deepika.A <110418508+DeepikaA2004@users.noreply.github.com>
2023-09-11 15:55:40 +0530

    Add files via upload

---
+++ Tank.java
@@ -0,0 +1,8 @@
+public class Tank {
+    public static void main(String[] args) {
+        double pi = 3.1415;
+        double r = 21.0000;
+        double v = (4.0 / 3.0) * pi * Math.pow(r, 3);
+        System.out.println(v);
+    }
+}
